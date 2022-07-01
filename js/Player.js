0f68985d37ec2b346aaf8231e40e3e8a7e1181e8
BethCollider <88354512+BethCollider@users.noreply.github.com>
2022-07-01 16:51:58 -0500

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,70 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+    //Declarar ranking y puntuacion
+    /*this.rank = 0;
+    this.score = 0;*/
+  }
+
+  addPlayer() {
+    var playerIndex = "players/player" + this.index;
+
+    if (this.index === 1) {
+      this.positionX = width / 2 - 100;
+    } else {
+      this.positionX = width / 2 + 100;
+    }
+
+    database.ref(playerIndex).set({
+      name: this.name,
+      positionX: this.positionX,
+      positionY: this.positionY,
+      //Se actualiza informacion del player
+      /*rank: this.rank,
+      score: this.score*/
+    });
+  }
+
+  getDistance() {
+    var playerDistanceRef = database.ref("players/player" + this.index);
+    playerDistanceRef.on("value", data => {
+      var data = data.val();
+      this.positionX = data.positionX;
+      this.positionY = data.positionY;
+    });
+  }
+
+  getCount() {
+    var playerCountRef = database.ref("playerCount");
+    playerCountRef.on("value", data => {
+      playerCount = data.val();
+    });
+  }
+
+  updateCount(count) {
+    database.ref("/").update({
+      playerCount: count
+    });
+  }
+
+  update() {
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).update({
+      positionX: this.positionX,
+      positionY: this.positionY,
+      //Se actualiza en la base de datos
+      /*rank: this.rank,
+      score: this.score*/
+    });
+  }
+
+  static getPlayersInfo() {
+    var playerInfoRef = database.ref("players");
+    playerInfoRef.on("value", data => {
+      allPlayers = data.val();
+    });
+  }
+}
